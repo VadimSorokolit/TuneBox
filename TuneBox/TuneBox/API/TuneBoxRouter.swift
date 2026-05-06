53784f2a24ff2ca12f8121ac8634ccde532bd322
Vadym Sorokolit <macintosh@ukr.net>
2026-05-06 16:13:26 +0300

    Added params property to parameters handling

--- TuneBox/TuneBox/API/TuneBoxRouter.swift
+++ TuneBox/TuneBox/API/TuneBoxRouter.swift
@@ -17,7 +17,7 @@ private struct Constants {
         static let defaultPath = "/tracks"
         static let downloadPath = "/download/track/%d/mp32/"
     }
-    
+
     struct Params {
         static let clientID = "client_id"
         static let tags = "tags"
@@ -26,17 +26,17 @@ private struct Constants {
         static let search = "search"
         static let offset = "offset"
     }
-    
+
     struct Values {
         static let popularityTotal = "popularity_total"
     }
-    
+
     static let mockAPIKey = "88888888"
     static let googleBaseURL = "google.com"
     static let apiKeyEnvName = "APIKey"
-    static let invalidURLMessage: String = "Invalid baseURL:"
-    static let warningMessage: String = "Using mock API key"
-    
+    static let invalidURLMessage = "Invalid baseURL:"
+    static let warningMessage = "Using mock API key"
+
     static var apiKey: String {
         if let key = ProcessInfo.processInfo.environment[Constants.apiKeyEnvName] {
             return key
@@ -45,7 +45,6 @@ private struct Constants {
             return Constants.mockAPIKey
         }
     }
-    
 }
 
 enum TuneBoxRouter {
@@ -58,83 +57,92 @@ enum TuneBoxRouter {
 // MARK: - TargetType Protocol
 
 extension TuneBoxRouter: TargetType {
-    
+
+    private typealias API = Constants.API
+    private typealias Params = Constants.Params
+
+    private var params: [String: Any] {
+        switch self {
+            case let .getTracksByGenre(genre, limit):
+                return [
+                    Params.clientID: Constants.apiKey,
+                    Params.tags: genre,
+                    Params.limit: limit
+                ]
+
+            case let .getPopularTracks(limit):
+                return [
+                    Params.clientID: Constants.apiKey,
+                    Params.order: Constants.Values.popularityTotal,
+                    Params.limit: limit
+                ]
+
+            case let .searchTracks(query, limit, offset):
+                return [
+                    Params.clientID: Constants.apiKey,
+                    Params.search: query,
+                    Params.limit: limit,
+                    Params.offset: offset
+                ]
+
+            case .getSongSize:
+                return [:]
+        }
+    }
+
     var baseURL: URL {
         let urlString: String
-        
+
         switch self {
             case .getSongSize:
-                urlString = Constants.API.storageURL
+                urlString = API.storageURL
+
             default:
-                urlString = Constants.API.baseURL
+                urlString = API.baseURL
         }
-        
+
         guard let url = URL(string: urlString) ?? URL(string: Constants.googleBaseURL) else {
             fatalError("\(Constants.invalidURLMessage) \(urlString)")
         }
-        
+
         return url
     }
-    
+
     var path: String {
         switch self {
             case .getSongSize(let id):
-                return String(format: Constants.API.downloadPath, id)
-                
+                return String(format: API.downloadPath, id)
+
             case .getTracksByGenre, .getPopularTracks, .searchTracks:
-                return Constants.API.defaultPath
+                return API.defaultPath
         }
     }
-    
+
     var method: Moya.Method {
         switch self {
             case .getSongSize:
                 return .head
+
             default:
                 return .get
         }
     }
-    
+
     var task: Task {
         switch self {
-            case let .getTracksByGenre(genre, limit):
-                return .requestParameters(
-                    parameters: [
-                        Constants.Params.clientID: Constants.apiKey,
-                        Constants.Params.tags: genre,
-                        Constants.Params.limit: limit
-                    ],
-                    encoding: URLEncoding.default
-                )
-                
-            case let .getPopularTracks(limit):
-                return .requestParameters(
-                    parameters: [
-                        Constants.Params.clientID: Constants.apiKey,
-                        Constants.Params.order: Constants.Values.popularityTotal,
-                        Constants.Params.limit: limit
-                    ],
-                    encoding: URLEncoding.default
-                )
-                
-            case let .searchTracks(query, limit, offset):
-                return .requestParameters(
-                    parameters: [
-                        Constants.Params.clientID: Constants.apiKey,
-                        Constants.Params.search: query,
-                        Constants.Params.limit: limit,
-                        Constants.Params.offset: offset
-                    ],
-                    encoding: URLEncoding.default
-                )
-                
             case .getSongSize:
                 return .requestPlain
+
+            default:
+                return .requestParameters(
+                    parameters: params,
+                    encoding: URLEncoding.default
+                )
         }
     }
-    
+
     var headers: [String: String]? {
         nil
     }
-    
+
 }
