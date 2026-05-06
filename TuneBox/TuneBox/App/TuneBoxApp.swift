9c6ce2d04a088fb0dffa3059a1c26bfcfe32170c
Vadym Sorokolit <macintosh@ukr.net>
2026-05-06 11:53:13 +0300

    Initial project setup: added Moya, SwiftLint, Custom Alerts

---
+++ TuneBox/TuneBox/App/TuneBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneBoxApp.swift
+//  TuneBox
+//
+//  Created by Vadim Sorokolit on 06.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TuneBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuneBox/TuneBoxTests/TuneBoxTests.swift
@@ -0,0 +1,19 @@
+//
+//  TuneBoxTests.swift
+//  TuneBoxTests
+//
+//  Created by Vadim Sorokolit on 06.05.2026.
+//
+
+import Testing
+@testable import TuneBox
+
+struct TuneBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
