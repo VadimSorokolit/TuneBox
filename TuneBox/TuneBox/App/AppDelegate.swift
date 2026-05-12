dc87e7210ff7acf37aef6177c16ed83bb7385f32
Vadym Sorokolit <macintosh@ukr.net>
2026-05-12 12:16:38 +0300

    Integrateed AppDelegate using UIApplicationDelegateAdaptor for lifecycle handling

---
+++ TuneBox/TuneBox/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  TuneBox
+//
+//  Created by Vadim Sorokolit on 12.05.2026.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        handleEventsForBackgroundURLSession identifier: String,
+        completionHandler: @escaping () -> Void
+    ) {}
+}
--- TuneBox/TuneBox/App/TuneBoxApp.swift
+++ TuneBox/TuneBox/App/TuneBoxApp.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 @main
 struct TuneBoxApp: App {
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
