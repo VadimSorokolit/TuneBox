c50ce9478e48bfa05fe71a086c4a597e75cec77f
Vadym Sorokolit <macintosh@ukr.net>
2026-06-02 13:20:51 +0300

    Added empy views launch and onboarding

---
+++ TuneBox/TuneBox/Features/Launch/Launch.swift
@@ -0,0 +1,16 @@
+//
+//  Launch.swift
+//  TuneBox
+//
+//  Created by Vadim Sorokolit on 02.06.2026.
+//
+
+import SwiftUI
+
+struct Launch: View {
+
+    var body: some View {
+        Text("Launch")
+    }
+
+}
---
+++ TuneBox/TuneBox/Features/Onboarding/Onboarding.swift
@@ -0,0 +1,16 @@
+//
+//  Onboarding.swift
+//  TuneBox
+//
+//  Created by Nintendo on 02.06.2026.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+
+    var body: some View {
+        Text("Onboarding")
+    }
+
+}
